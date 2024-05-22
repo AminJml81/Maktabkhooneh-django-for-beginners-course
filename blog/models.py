4d8ef4d1661a7b761204cb97cbc7bbf846164dcf
Amin <Aminbest1381@gmail.com>
2024-05-22 23:43:21 +0330

    added Contact and Post models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    # to do 
+    # image, author, tag, category
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    counted_views = models.PositiveIntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
