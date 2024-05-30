d008fd819d84f3aed1057145144f0a333469841a
Amin <Aminbest1381@gmail.com>
2024-05-30 20:42:44 +0330

    finished lesson 6 added better functionalities for html templates

--- blog/admin.py
+++ blog/admin.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post
+from .models import Post, Category
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_at' 
     empty_value_display = '-empty-'
-    list_display =  ('title', 'counted_views', 'status', 'published_date', 'created_at')
-    list_filter = ('status', )
+    list_display =  ('title', 'author' ,'counted_views', 'status', 'published_date', 'created_at')
+    list_filter = ('status', 'author')
     ordering = ('-created_at',)
     search_fields = ('title', 'content')
-# admin.site.register(Post, PostAdmin)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    
+    def __str__(self) -> str:
+        return self.name
+    
 class Post(models.Model):
     # to do 
-    # image, author, tag, category
+    #tag,
+    image = models.ImageField(upload_to = 'blog/', default='blog/default.jpj')
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
+    category = models.ManyToManyField(Category)
     counted_views = models.PositiveIntegerField(default=0)
     status = models.BooleanField(default=False)
     published_date = models.DateTimeField(null=True)
